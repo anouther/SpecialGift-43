644dc5ce8b0243bdea8f8a907f8556a88e15d6c1
anouther <155570107+anouther@users.noreply.github.com>
2025-06-05 19:55:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Tomo, Happy Mensive').split('');
+        const titles = ('Happy birthday to the one and only, my Lee Haechan').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Sayangku, i love you the most. Makasih udah buat aku bahagia terus selama ini";
+            subtitle.innerText = "Hey love, this is your day, i hope it's filled with laughter, love, and everything that makes your heart light up. You've brought so much warmth into my life. I'm so proud of you, and so lucky to witness your magic. So here's to you, my favorite Haechan. May your day be as bright as your smile, and your year be as unforgettable as you are. 🩷";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
